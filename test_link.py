62d2c0deeee351cb1ef642f3d4fd70c830b01fae
Sijis Aviles <sijis.aviles+github@gmail.com>
2016-12-02 16:55:55 -0600

    fixed test cases

--- test_link.py
+++ test_link.py
@@ -1,5 +1,5 @@
 import link
-from errbot.backends.test import testbot, push_message, pop_message
+from errbot.backends.test import testbot
 from errbot import plugin_manager
 
 
@@ -7,30 +7,32 @@ class TestLink(object):
     extra_plugin_dir = '.'
 
     def test_link_add(self, testbot):
-        push_message('!link add github http://github.com')
-        assert 'Successfully added "github"' in pop_message()
+        testbot.push_message('!link add github http://github.com')
+        assert 'Successfully added "github"' in testbot.pop_message()
 
-    def test_link(self, testbot):
-        push_message('!link github')
-        assert 'http://github.com' in pop_message()
+    def test_link_available(self, testbot):
+        self.test_link_add(testbot)
+        testbot.push_message('!link github')
+        assert 'http://github.com' in testbot.pop_message()
 
     def test_link_delete(self, testbot):
-        push_message('!link delete github')
-        assert 'Successfully deleted "github"' in pop_message()
+        self.test_link_add(testbot)
+        testbot.push_message('!link delete github')
+        assert 'Successfully deleted "github"' in testbot.pop_message()
 
     def test_link_list(self, testbot):
         self.test_link_add(testbot)
-        push_message('!link list')
-        assert 'Available keys: github' in pop_message()
+        testbot.push_message('!link list')
+        assert 'Available keys: github' in testbot.pop_message()
 
     def test_link_add_fail(self, testbot):
-        push_message('!link add notfound')
-        assert 'Wrong number of arguments. You need 2.' in pop_message()
+        testbot.push_message('!link add wrong_args')
+        assert 'Wrong number of arguments. You need 2.' in testbot.pop_message()
 
     def test_link_fail(self, testbot):
-        push_message('!link notfound')
-        assert 'I have no link for notfound' in pop_message()
+        testbot.push_message('!link notfound')
+        assert 'I have no link for notfound' in testbot.pop_message()
 
     def test_link_delete_fail(self, testbot):
-        push_message('!link delete notfound')
-        assert 'I have no record of notfound' in pop_message()
+        testbot.push_message('!link delete notfound')
+        assert 'I have no record of notfound' in testbot.pop_message()
